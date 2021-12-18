09ade16fbd8bce1c54da2fc043046f76427c67e4
unknown <hoyzington@gmail.com>
2021-12-17 18:14:57 -0600

    further implement & refactor; add temp game route

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,3 +1,16 @@
-const MainContainer = () => (<div id='body-container'></div>);
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Game from './game/Game';
+
+function MainContainer() {
+    return (
+        <div id='body-container'>
+            <Routes>
+                <Route path="/home" element={<Home />}/>
+                <Route path="/game" element={<Game />}/>
+            </Routes>
+        </div>
+    );
+} 
 
 export default MainContainer;
